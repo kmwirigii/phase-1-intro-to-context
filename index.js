7da1b1c6b4342e4ec3ff8b70efc691fce3723c65
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-03-24 13:52:35 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,107 @@
-// Your code here
+/* Your Code Here */
+// helpers.js
+
+// Function to create a single employee record
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+// Function to create multiple employee records
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+// Function to create a "TimeIn" event
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    });
+    return this;
+}
+
+// Function to create a "TimeOut" event
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    });
+    return this;
+}
+
+// Function to calculate hours worked on a specific date
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(e => e.date === date);
+    const timeOut = this.timeOutEvents.find(e => e.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+// Function to calculate wages earned on a specific date
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+// Function to calculate total wages for an employee
+function allWagesFor() {
+    return this.timeInEvents.reduce((total, event) => {
+        return total + wagesEarnedOnDate.call(this, event.date);
+    }, 0);
+}
+
+// Function to find an employee by first name
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+// Function to calculate total payroll for all employees
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, employee) => {
+        return total + allWagesFor.call(employee);
+    }, 0);
+}
+
+// Export all functions
+module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    AllWagesFor,
+    findEmployeeByFirstName,
+    calculatePayroll
+};
+
+
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+const AllWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    const payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable
+}
